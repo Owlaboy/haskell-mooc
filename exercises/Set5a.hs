3e7865968154d004a6e1a9012364ef2fd7c1718a
Owlaboy <andrei.preotesa.ap@gmail.com>
2025-03-10 00:53:42 +0200

    Finished lecture 5 exercises

--- exercises/Set5a.hs
+++ exercises/Set5a.hs
@@ -13,6 +13,7 @@ import Mooc.Todo
 --
 -- The constructors don't need any fields.
 
+data Vehicle = Bike | Bus | Tram | Train 
 
 ------------------------------------------------------------------------------
 -- Ex 2: Define the type BusTicket that can represent values like these:
@@ -20,6 +21,7 @@ import Mooc.Todo
 --  - MonthlyTicket "January"
 --  - MonthlyTicket "December"
 
+data BusTicket = SingleTicket | MonthlyTicket String
 
 ------------------------------------------------------------------------------
 -- Ex 3: Here's the definition for a datatype ShoppingEntry that
@@ -48,7 +50,7 @@ twoBananas = MkShoppingEntry "Banana" 1.1 2
 --   totalPrice twoBananas   ==> 2.2
 
 totalPrice :: ShoppingEntry -> Double
-totalPrice = todo
+totalPrice (MkShoppingEntry name p c) = p * fromIntegral c
 
 -- buyOneMore should increment the count in an entry by one
 --
@@ -56,7 +58,7 @@ totalPrice = todo
 --   buyOneMore twoBananas    ==> MkShoppingEntry "Banana" 1.1 3
 
 buyOneMore :: ShoppingEntry -> ShoppingEntry
-buyOneMore = todo
+buyOneMore (MkShoppingEntry name p c) = MkShoppingEntry name p (c+1)
 
 ------------------------------------------------------------------------------
 -- Ex 4: define a datatype Person, which should contain the age (an
@@ -65,28 +67,28 @@ buyOneMore = todo
 -- Also define a Person value fred, and the functions getAge, getName,
 -- setAge and setName (see below).
 
-data Person = PersonUndefined
+data Person = PersonUndefined { name :: String, age :: Int}
   deriving Show
 
 -- fred is a person whose name is Fred and age is 90
 fred :: Person
-fred = todo
+fred = PersonUndefined "Fred" 90
 
 -- getName returns the name of the person
 getName :: Person -> String
-getName p = todo
+getName  = name 
 
 -- getAge returns the age of the person
 getAge :: Person -> Int
-getAge p = todo
+getAge  = age 
 
 -- setName takes a person and returns a new person with the name changed
 setName :: String -> Person -> Person
-setName name p = todo
+setName name p = PersonUndefined name (age p)
 
 -- setAge does likewise for age
 setAge :: Int -> Person -> Person
-setAge age p = todo
+setAge age p = PersonUndefined (name p) age
 
 ------------------------------------------------------------------------------
 -- Ex 5: define a datatype Position which contains two Int values, x
@@ -96,27 +98,27 @@ setAge age p = todo
 --   getY (up (up origin))    ==> 2
 --   getX (up (right origin)) ==> 1
 
-data Position = PositionUndefined
+data Position = PositionUndefined { xVal::Int, yVal::Int}
 
 -- origin is a Position value with x and y set to 0
 origin :: Position
-origin = todo
+origin = PositionUndefined 0 0
 
 -- getX returns the x of a Position
 getX :: Position -> Int
-getX = todo
+getX = xVal
 
 -- getY returns the y of a position
 getY :: Position -> Int
-getY = todo
+getY = yVal
 
 -- up increases the y value of a position by one
 up :: Position -> Position
-up = todo
+up p = PositionUndefined (xVal p) (yVal p +1)
 
 -- right increases the x value of a position by one
 right :: Position -> Position
-right = todo
+right p = PositionUndefined (xVal p +1) (yVal p)
 
 ------------------------------------------------------------------------------
 -- Ex 6: Here's a datatype that represents a student. A student can
@@ -131,7 +133,11 @@ data Student = Freshman | NthYear Int | Graduated
 -- graduated student stays graduated even if he studies.
 
 study :: Student -> Student
-study = todo
+study Freshman = NthYear 1
+study (NthYear 7) = Graduated
+study (NthYear a) = NthYear (a+1)
+study Graduated = Graduated
+
 
 ------------------------------------------------------------------------------
 -- Ex 7: define a datatype UpDown that represents a counter that can
@@ -150,25 +156,27 @@ study = todo
 -- get (tick (tick (toggle (tick zero))))
 --   ==> -1
 
-data UpDown = UpDownUndefined1 | UpDownUndefined2
+data UpDown = Uppies { val :: Int } | Downies { val :: Int }
 
 -- zero is an increasing counter with value 0
 zero :: UpDown
-zero = todo
+zero = Uppies 0
 
 -- get returns the counter value
 get :: UpDown -> Int
-get ud = todo
+get ud = (val ud)
 
 -- tick increases an increasing counter by one or decreases a
 -- decreasing counter by one
 tick :: UpDown -> UpDown
-tick ud = todo
+tick (Uppies v) = Uppies (v+1)
+tick (Downies v) = Downies (v-1)
 
 -- toggle changes an increasing counter into a decreasing counter and
 -- vice versa
 toggle :: UpDown -> UpDown
-toggle ud = todo
+toggle (Uppies v) = Downies v
+toggle (Downies v) = Uppies v
 
 ------------------------------------------------------------------------------
 -- Ex 8: you'll find a Color datatype below. It has the three basic
@@ -198,16 +206,26 @@ data Color = Red | Green | Blue | Mix Color Color | Invert Color
   deriving Show
 
 rgb :: Color -> [Double]
-rgb col = todo
+rgb Red = [1.0,0.0,0.0]
+rgb Green = [0.0,1.0,0.0]
+rgb Blue = [0.0,0.0,1.0]
 
-------------------------------------------------------------------------------
+rgb (Mix col1 col2) = mixer (rgb col1) (rgb col2)
+
+rgb (Invert col) = map (1.0 - ) (rgb col)
+
+
+
+mixer (x:xs) (y:ys) = ( (x+y)/ 2.0):mixer xs ys
+mixer _ _ = [] 
+-----------------------------------------------------------------------------
 -- Ex 9: define a parameterized datatype OneOrTwo that contains one or
 -- two values of the given type. The constructors should be called One and Two.
 --
 -- Examples:
 --   One True         ::  OneOrTwo Bool
 --   Two "cat" "dog"  ::  OneOrTwo String
-
+data OneOrTwo a = One a | Two a a
 
 ------------------------------------------------------------------------------
 -- Ex 10: define a recursive datatype KeyVals for storing a set of
@@ -228,14 +246,16 @@ rgb col = todo
 -- Also define the functions toList and fromList that convert between
 -- KeyVals and lists of pairs.
 
-data KeyVals k v = KeyValsUndefined
+data KeyVals k v = Empty | Pair k v (KeyVals k v)
   deriving Show
 
 toList :: KeyVals k v -> [(k,v)]
-toList = todo
+toList (Pair k v kv) = (k,v):toList kv
+toList Empty = [] 
 
 fromList :: [(k,v)] -> KeyVals k v
-fromList = todo
+fromList (x:xs) =  Pair (fst x) (snd x) (fromList xs)
+fromList [] = Empty 
 
 ------------------------------------------------------------------------------
 -- Ex 11: The data type Nat is the so called Peano
@@ -252,10 +272,18 @@ data Nat = Zero | PlusOne Nat
   deriving (Show,Eq)
 
 fromNat :: Nat -> Int
-fromNat n = todo
+fromNat (PlusOne n) = 1+fromNat n
+fromNat Zero = 0
 
 toNat :: Int -> Maybe Nat
-toNat z = todo
+toNat z 
+  | z < 0 = Nothing
+  | z >= 0 = Just (recursor z)
+  | otherwise = Nothing
+
+recursor :: (Eq t, Num t) => t -> Nat
+recursor 0 = Zero
+recursor n = PlusOne (recursor (n-1))
 
 ------------------------------------------------------------------------------
 -- Ex 12: While pleasingly simple in its definition, the Nat datatype is not
@@ -315,10 +343,21 @@ inc (O b) = I b
 inc (I b) = O (inc b)
 
 prettyPrint :: Bin -> String
-prettyPrint = todo
+prettyPrint End = ""
+prettyPrint (O b) = prettyPrint b ++ "0"
+prettyPrint (I b) = prettyPrint b ++ "1"
 
 fromBin :: Bin -> Int
-fromBin = todo
+fromBin b = fromBin' b 0
+
+fromBin' End _ = 0
+fromBin' (O b) d = fromBin' b (d+1)
+fromBin' (I b) d = 2^d + fromBin' b (d+1)
 
 toBin :: Int -> Bin
-toBin = todo
+toBin n = toBin' n 0 End
+
+toBin' n d b 
+  | n == 0 = O End
+  | n == d = b
+  | otherwise = toBin' n (d+1) (inc b)
--- exercises/Set5b.hs
+++ exercises/Set5b.hs
@@ -15,7 +15,8 @@ data Tree a = Empty | Node a (Tree a) (Tree a)
 -- because the tree might be empty (i.e. just a Empty)
 
 valAtRoot :: Tree a -> Maybe a
-valAtRoot t = todo
+valAtRoot Empty = Nothing
+valAtRoot (Node a b c) = Just a
 
 ------------------------------------------------------------------------------
 -- Ex 2: compute the size of a tree, that is, the number of Node
@@ -26,7 +27,9 @@ valAtRoot t = todo
 --   treeSize (Node 3 (Node 7 Empty Empty) (Node 1 Empty Empty))  ==>  3
 
 treeSize :: Tree a -> Int
-treeSize t = todo
+treeSize Empty  = 0
+treeSize (Node a b c) = 1 + treeSize b + treeSize c
+
 
 ------------------------------------------------------------------------------
 -- Ex 3: get the largest value in a tree of positive Ints. The
@@ -37,7 +40,8 @@ treeSize t = todo
 --   treeMax (Node 3 (Node 5 Empty Empty) (Node 4 Empty Empty))  ==>  5
 
 treeMax :: Tree Int -> Int
-treeMax = todo
+treeMax Empty = 0
+treeMax (Node a b c) = max a (max (treeMax b) (treeMax c))
 
 ------------------------------------------------------------------------------
 -- Ex 4: implement a function that checks if all tree values satisfy a
@@ -49,7 +53,8 @@ treeMax = todo
 --   allValues (>0) (Node 1 Empty (Node 0 Empty Empty))  ==>  False
 
 allValues :: (a -> Bool) -> Tree a -> Bool
-allValues condition tree = todo
+allValues condition (Node a b c) = condition a && allValues condition b && allValues condition c
+allValues condition Empty = True
 
 ------------------------------------------------------------------------------
 -- Ex 5: implement map for trees.
@@ -61,7 +66,8 @@ allValues condition tree = todo
 --   ==> (Node 2 (Node 3 Empty Empty) (Node 4 Empty Empty))
 
 mapTree :: (a -> b) -> Tree a -> Tree b
-mapTree f t = todo
+mapTree f (Node a b c) = Node (f a) (mapTree f b) (mapTree f c)
+mapTree f Empty = Empty
 
 ------------------------------------------------------------------------------
 -- Ex 6: given a value and a tree, build a new tree that is the same,
@@ -105,7 +111,10 @@ mapTree f t = todo
 --                 (Node 3 Empty Empty))
 
 cull :: Eq a => a -> Tree a -> Tree a
-cull val tree = todo
+cull val (Node a b c) = if val == a
+  then Empty
+  else Node a (cull val b) (cull val c)
+cull val Empty = Empty
 
 ------------------------------------------------------------------------------
 -- Ex 7: check if a tree is ordered. A tree is ordered if:
@@ -147,7 +156,8 @@ cull val tree = todo
 --                     (Node 3 Empty Empty))   ==>   True
 
 isOrdered :: Ord a => Tree a -> Bool
-isOrdered = todo
+isOrdered (Node a b c) = allValues (<a) b && allValues (>a) c && isOrdered b && isOrdered c
+isOrdered Empty = True
 
 ------------------------------------------------------------------------------
 -- Ex 8: a path in a tree can be represented as a list of steps that
@@ -166,8 +176,13 @@ data Step = StepL | StepR
 --   walk [StepL,StepL] (Node 1 (Node 2 Empty Empty) Empty)  ==>  Nothing
 
 walk :: [Step] -> Tree a -> Maybe a
-walk = todo
+walk (x:xs) (Node a b c)
+  | x == StepL = walk xs b
+  | x == StepR = walk xs c
+  | otherwise = Just a
 
+walk [] (Node a b c) = Just a
+walk _ Empty = Nothing
 ------------------------------------------------------------------------------
 -- Ex 9: given a tree, a path and a value, set the value at the end of
 -- the path to the given value. Since Haskell datastructures are
@@ -187,8 +202,12 @@ walk = todo
 --   set [StepL,StepR] 1 (Node 0 Empty Empty)  ==>  (Node 0 Empty Empty)
 
 set :: [Step] -> a -> Tree a -> Tree a
-set path val tree = todo
-
+set (x:xs) val (Node a b c)
+  | x == StepL = Node a (set xs val b) c
+  | x == StepR = Node a b (set xs val c)
+  | otherwise = Empty
+set [] val (Node a b c) = Node val b c
+set _ _ _ = Empty
 ------------------------------------------------------------------------------
 -- Ex 10: given a value and a tree, return a path that goes from the
 -- root to the value. If the value doesn't exist in the tree, return Nothing.
@@ -203,4 +222,23 @@ set path val tree = todo
 --                    (Node 5 Empty Empty))                     ==>  Just [StepL,StepR]
 
 search :: Eq a => a -> Tree a -> Maybe [Step]
-search = todo
+search val (Node a b c)
+  | checker val (Node a b c) = Just (search' val (Node a b c))
+  | otherwise = Nothing
+
+
+search val Empty = Nothing
+
+
+search' val (Node a b c)
+  | val == a = []
+  | checker val b = StepL : search' val b
+  | otherwise = StepR : search' val c
+
+search' val Empty = []
+
+
+checker val (Node a b c)
+  | val == a = True
+  | otherwise = checker val b || checker val c
+checker val Empty = False
